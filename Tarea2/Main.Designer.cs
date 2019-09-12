633c93ad04eb7f786382b28b6ab16ec1da7679cb
Misael_AJ <missael133@gmail.com>
2019-09-11 22:13:17 -0500

    - Need fix some bugs

---
+++ Tarea2/Main.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Tarea2
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.btnSuspend = new System.Windows.Forms.Button();
+            this.mnsMain = new System.Windows.Forms.MenuStrip();
+            this.stripMAcerca = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripMCreditos = new System.Windows.Forms.ToolStripMenuItem();
+            this.listview1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.mnsMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(345, 45);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(155, 32);
+            this.btnResume.TabIndex = 3;
+            this.btnResume.Text = "Activar";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // btnSuspend
+            // 
+            this.btnSuspend.Location = new System.Drawing.Point(345, 92);
+            this.btnSuspend.Name = "btnSuspend";
+            this.btnSuspend.Size = new System.Drawing.Size(155, 30);
+            this.btnSuspend.TabIndex = 2;
+            this.btnSuspend.Text = "Suspender";
+            this.btnSuspend.UseVisualStyleBackColor = true;
+            this.btnSuspend.Click += new System.EventHandler(this.btnSuspend_Click);
+            // 
+            // mnsMain
+            // 
+            this.mnsMain.BackColor = System.Drawing.SystemColors.Control;
+            this.mnsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stripMAcerca,
+            this.stripMCreditos});
+            this.mnsMain.Location = new System.Drawing.Point(0, 0);
+            this.mnsMain.Name = "mnsMain";
+            this.mnsMain.Size = new System.Drawing.Size(526, 24);
+            this.mnsMain.TabIndex = 0;
+            this.mnsMain.Text = "menuStrip1";
+            // 
+            // stripMAcerca
+            // 
+            this.stripMAcerca.Name = "stripMAcerca";
+            this.stripMAcerca.Size = new System.Drawing.Size(72, 20);
+            this.stripMAcerca.Text = "Acerda de";
+            this.stripMAcerca.Click += new System.EventHandler(this.tsmiAbout_Click);
+            // 
+            // stripMCreditos
+            // 
+            this.stripMCreditos.Name = "stripMCreditos";
+            this.stripMCreditos.Size = new System.Drawing.Size(63, 20);
+            this.stripMCreditos.Text = "Creditos";
+            this.stripMCreditos.Click += new System.EventHandler(this.tsmiCredit_Click);
+            // 
+            // listview1
+            // 
+            this.listview1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listview1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listview1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.listview1.FullRowSelect = true;
+            this.listview1.GridLines = true;
+            this.listview1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listview1.HideSelection = false;
+            this.listview1.HoverSelection = true;
+            this.listview1.Location = new System.Drawing.Point(22, 36);
+            this.listview1.MultiSelect = false;
+            this.listview1.Name = "listview1";
+            this.listview1.Size = new System.Drawing.Size(300, 243);
+            this.listview1.TabIndex = 0;
+            this.listview1.UseCompatibleStateImageBehavior = false;
+            this.listview1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Estado";
+            this.columnHeader2.Width = 150;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 306);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listview1);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnSuspend);
+            this.Controls.Add(this.mnsMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.mnsMain;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FCFS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.mnsMain.ResumeLayout(false);
+            this.mnsMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Button btnSuspend;
+        private System.Windows.Forms.MenuStrip mnsMain;
+        private System.Windows.Forms.ToolStripMenuItem stripMAcerca;
+        private System.Windows.Forms.ToolStripMenuItem stripMCreditos;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView listview1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
